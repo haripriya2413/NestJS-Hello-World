9b9e3c8d56d3b75943b89dc9821775fdecc68f69
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-17 18:22:33 +0530

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! PearlThought!!';
+    return 'Hello World! Welcome to PearlThought!!';
   }
 
   getPing(): string {
