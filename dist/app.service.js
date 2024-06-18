5a45d646d659c023733b04db6206814af987582a
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-06-18 08:55:50 +0530

    Update app.service.js

--- dist/app.service.js
+++ dist/app.service.js
@@ -9,7 +9,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 let AppService = class AppService {
     getHello() {
-        return 'Hello World! Huuuray!!!';
+        return 'Hello World! Welcome to PearlThoughts!!!';
     }
     getPing() {
         return 'Pong!';
